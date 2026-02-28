1062c9bad109e06d73707c44792d022e8e1ed064
xBalzerian <balzgaming77@gmail.com>
2026-02-28 09:53:11 +0800

    Use GOOGLE_SERVICE_ACCOUNT_KEY env var (base64)

--- google-service.js
+++ google-service.js
@@ -1,17 +1,16 @@
 const { google } = require('googleapis');
 
-// Build service account from env vars
-const SERVICE_ACCOUNT = {
-  type: 'service_account',
-  project_id: process.env.GOOGLE_PROJECT_ID || 'wordpress-claw',
-  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID || '',
-  private_key: (process.env.GOOGLE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
-  client_email: process.env.GOOGLE_CLIENT_EMAIL || '',
-  client_id: process.env.GOOGLE_CLIENT_ID || '',
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs'
-};
+// Get service account from env var (base64 encoded JSON)
+let SERVICE_ACCOUNT = {};
+try {
+  const base64Key = process.env.GOOGLE_SERVICE_ACCOUNT_KEY || '';
+  if (base64Key) {
+    const decoded = Buffer.from(base64Key, 'base64').toString('utf8');
+    SERVICE_ACCOUNT = JSON.parse(decoded);
+  }
+} catch (e) {
+  console.error('Failed to decode service account key:', e.message);
+}
 
 class GoogleService {
   constructor() {
@@ -25,7 +24,6 @@ class GoogleService {
     });
     
     this.docs = google.docs({ version: 'v1', auth: this.auth });
-    this.drive = google.drive({ version: 'v3', auth: this.auth });
     this.sheets = google.sheets({ version: 'v4', auth: this.auth });
   }
 
